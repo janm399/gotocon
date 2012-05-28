f514b5ec3ab01ac36a7f992a69d13ac1cd3b5ec8
Jan Machacek <janmachacek@janmachacek.local>
2012-05-28 12:05:24 +0100

    uopdates

---
+++ src/main/groovy/A.groovy
@@ -0,0 +1,16 @@
+
+/**
+ * @author janmachacek
+ */
+class A {
+
+  static def main(args) {
+    def apples = 5
+    def oranges = "oranges"
+
+    def fruitSalad = apples * oranges
+
+    println(fruitSalad)
+  }
+
+}
